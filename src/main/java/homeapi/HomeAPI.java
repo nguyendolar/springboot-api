86258e3a246a99fce45501716d5935f929b75873
nguyendolar <nguyencaonguyencmu@gmail.com>
2021-07-12 17:01:14 +0700

    first commit

---
+++ src/main/java/homeapi/HomeAPI.java
@@ -0,0 +1,13 @@
+package homeapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeAPI {
+	@GetMapping("/api/test")
+	public ResponseEntity<String> testSpringBoot() {
+		return ResponseEntity.ok("ok");
+	}
+}
